8f1a9acee9e29a89c025e1be3f5961a8cdb34cad
Gusta-Alves <gustavoalves1010@gmail.com>
2022-08-21 19:05:36 -0300

    skeleton layout

--- src/app/views/characters/components/card-character/card-character.component.ts
+++ src/app/views/characters/components/card-character/card-character.component.ts
@@ -1,18 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { ICharacter } from 'src/app/models/character.interface';
 
 @Component({
   selector: 'app-card-character',
   templateUrl: './card-character.component.html',
-  styleUrls: ['./card-character.component.scss']
+  styleUrls: ['./card-character.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CardCharacterComponent implements OnInit {
+export class CardCharacterComponent {
 
   @Input() public Character: ICharacter = {} as ICharacter;
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public isLoading: boolean = false;
+  
+  randomBetween(min: number = 25, max: number = 75, round: boolean = false): number | null{
+    if(!this.isLoading) return null;
+    return round ? Math.round((Math.random() * (max - min)) + min) : (Math.random() * (max - min)) + min;
   }
 
 }
