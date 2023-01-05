12a0be90f50207521ace0dc09a2b3f09296dae6a
Gustavo Alves <gustavoalves1010@gmail.com>
2023-01-05 15:24:14 -0300

    created episodes component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'home', loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule), data: {title: 'Gallery'} },
   { path: 'characters', loadChildren: () => import('./views/characters/characters.module').then(m => m.CharactersModule), data: {title: 'Characteres'} },
+  { path: 'episodes', loadChildren: () => import('./views/episodes/episodes.module').then(m => m.EpisodesModule), data: {title: 'Episodes'} },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
